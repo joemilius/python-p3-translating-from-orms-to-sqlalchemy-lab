8f926d9aa35b4622820d3b75f0e702b941fdadf4
Joe Milius <joemilius@gmail.com>
2023-01-14 01:25:57 -0700

    done

--- app/dog.py
+++ app/dog.py
@@ -1,27 +1,42 @@
 from sqlalchemy import create_engine
 
+from models import Dog
+
 engine = create_engine('sqlite:///:memory:')
 
 def create_table(base):
+    base.metadata.create_all(engine)
+    return engine
     pass
 
 def save(session, dog):
+    session.add(dog)
+    session.commit()
+    return session
     pass
 
-def new_from_db(session):
+def new_from_db(session, row):
+    return session.query(Dog).filter_by(id = row.id).first()
     pass
 
 def get_all(session):
+    return [dog for dog in session.query(Dog)]
     pass
 
 def find_by_name(session, name):
+    return session.query(Dog).filter_by(name = name).first()
     pass
 
 def find_by_id(session, id):
+    return session.query(Dog).filter_by(id = id).first()
     pass
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
     pass
 
 def update_breed(session, dog, breed):
+    dog.breed = breed
+    session.commit()
+    return session
     pass
--- app/models.py
+++ app/models.py
@@ -6,4 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Dog(Base):
+    __tablename__ = 'dogs'
+
+    # Index('index_name', 'name')
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    breed = Column(String())
     pass
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    
